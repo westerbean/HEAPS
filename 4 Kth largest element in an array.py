eab167b79c789c02083a050dff7885e9f67bccf1
westerbean <femiaderewa@icloud.com>
2024-07-08 08:38:43 +0100

    added Heap algorithms

---
+++ 4 Kth largest element in an array.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        k = len(nums) - k
+
+        def quickSelect(l, r):
+            pivot, p = nums[r], l
+            for i in range(l, r):
+                if nums[i] <= pivot:
+                    nums[i], nums[r] = nums[r], nums[i]
+                    p += 1
+
+            nums[p], nums[r] = nums[r], nums[p]
+
+            if p > k:
+                return quickSelect(l, p - 1)
+            elif p < k:
+                return quickSelect(p + 1, r)
+            else:
+                return nums[p]
+
+        return quickSelect(0, len(nums) - 1)
+
+
+# Or you can sort nums and find the kth largest number beginning from the end
+#
+# nums.sort()
+# return nums[len(nums) - k]
